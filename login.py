dea8b9ce2705c3e5fc45470e9dc4d0f77ca46f4e
EricZhoa <48925971+EricZhoa@users.noreply.github.com>
2019-12-15 21:38:02 +0000

    A LoginRegister manager class created by Yifeng Zhao

--- login.py
+++ login.py
@@ -1,3 +1,9 @@
-class Login:
+class LoginRegister_manager:
     # This class is created by Yifeng Zhao
-    pass()
+    def __init__(self, User_name, Password):
+        self.User_name = "username"
+        self.Password = "password"
+
+    def Login_validation(self, User_name, Password):
+        if self.User_name == User_name and self.Password == Password:
+            return True
