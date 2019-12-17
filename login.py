944d54c758dea57ccd6fbf2553a41141999611ed
EricZhoa <48925971+EricZhoa@users.noreply.github.com>
2019-12-17 22:16:27 +0000

    a comment is added by Yifeng Zhao

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 # This class is created by Yifeng Zhao
 # Student Number: 17077657
-
+# a comment is added by Yifeng Zhao
 class LoginRegister_manager:
 
     def __init__(self, User_name, Password):
