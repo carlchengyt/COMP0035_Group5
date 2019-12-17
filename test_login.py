3d7b7f471d38c66cd1429f56a728d68fd7bfa4ed
EricZhoa <48925971+EricZhoa@users.noreply.github.com>
2019-12-17 18:49:56 +0000

    A test written by Yifeng Zhao

---
+++ test_login.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from login import LoginRegister_manager
+
+class TestLoginRegister_manager(TestCase):
+
+    def test_login_validation(self):
+        self.assertEqual(LoginRegister_manager("Eric","123abc").Login_validation("Eric","123abc"),True)
+
+    def test_login_validation_false(self):
+        self.assertEqual(LoginRegister_manager("Eric","123abc").Login_validation("Eric","abc123"),False)
+
