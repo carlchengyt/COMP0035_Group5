6507fab58d1d5cd6c95972fe12f449ab0a5abdad
carlchengyt <47320023+carlchengyt@users.noreply.github.com>
2019-12-17 21:45:00 +0000

    New test on user class

--- test_user.py
+++ test_user.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
+from user import User
 
 
 class TestUser(TestCase):
-    def test_add_user(self):
-        self.fail()
+    def test_add_user_info(self):
+        self.assertIs(User('Username').add_user_info('a comment'), 'a comment')
+        self.assertNotEqual(User('Username').add_user_info('a comment'), 'not a comment')
+
+
--- user.py
+++ user.py
@@ -1,7 +1,9 @@
 class User(object):
     def __init__(self, username):
         self._username = username
+        self._user_info = None
 
-
-    def add_user(self):
-        pass
+    def add_user_info(self,user_info):
+        if user_info is not None:
+            self._user_info = user_info
+            return user_info
