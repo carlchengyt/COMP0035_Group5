51e3360e000c3626b61126e9fd38eb892ccca933
rayomniscience <shirui.li.17@ucl.ac.uk>
2019-12-19 00:04:35 +0800

    commit new file

---
+++ comment.py
@@ -0,0 +1,21 @@
+# This class is created by SHIRUI LI
+# Student Number: 17050059
+
+class Comment:
+    def __init__(self, comment_id, content):
+        self.comment_id = comment_id
+        self.comment_content = content
+
+    def userstory_comment(self, comment_id, content):
+        if self.comment_id == comment_id:
+            self.comment_content = content
+            return True
+        else:
+            return False
+
+    def backlog_comment(self, comment_id, content):
+        if self.comment_id == comment_id:
+            self.comment_content = content
+            return True
+        else:
+            return False
---
+++ test_comment.py
@@ -0,0 +1,15 @@
+# This Team class is created by SHIRUI LI
+# Student Number: 17050059
+
+from unittest import TestCase
+from comment import Comment
+
+class TestComment(TestCase):
+
+    def test_userstory_comment_validation(self):
+        self.assertEqual(Comment('123', 'abc').userstory_comment('123', 'abc'), True)
+
+    def test_backlog_comment_false(self):
+        self.assertEqual(Comment('123', 'abc').backlog_comment('123', 'abc'), True)
+
+
